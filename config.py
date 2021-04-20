d0cff5629e9a58098fc8225b78e22f8de1f392f0
Krzysztof Nawrocki <krzysztofnawrocki.it@gmail.com>
2021-04-20 23:45:01 +0200

    My first version of app

---
+++ config.py
@@ -0,0 +1,43 @@
+########################## PLIK KONFIGURACYJNY ################################
+
+# miejsce zapisu faktury na dysku np. c:/
+path = " "
+# miejsce wystawienia faktury np. Poznań
+place = " "
+
+
+################### DANE SPRZEDAJĄCEJGO (WYSTAWIAJĄCY FAKTURĘ) ################
+
+# Nazwa firmy wystawiającej fakturę
+name_1 = " "
+# Adres firmy (ulica i numer budynku)
+adress_1 = " "
+# Adres firmy c.d. (kod pocztowy i miejscowość)
+additional_adress_1 = " "
+# NIP firmy wystawiającej fakturę
+nip_1 = " "
+# Adres email firmy wystawiającej fakturę
+mail_1 = " "
+# Telefon firmy wystawiającej fakturę
+tel_1 = " "
+
+################### DANE KUPUJĄCEGO (OTRZYMUJĄCY FAKTURĘ) ################
+
+# Nazwa firmy otrzymującej fakturę
+name_2 = " "
+# Adres firmy (ulica i numer budynku)
+adress_2 = " "
+# Adres firmy c.d. (kod pocztowy i miejscowość)
+additional_adress_2 = " "
+# NIP firmy otrzymującej fakturę
+nip_2 = " "
+
+######################### DANE WYSTAWIAJĄCEGO FAKTURĘ ##########################
+
+# Nazwa banku na jaki ma być zlecona zapłata za fakturę
+bank_name = " "
+# Numer konta na jaki ma być zlecona zapłata za fakturę
+bank_account = " "
+
+# Imię i nazwisko osoby upoważnionej do wystawienia faktury
+name_of_issuing = " "
---
+++ main.py
@@ -0,0 +1,162 @@
+from docx import Document
+from docx.enum.table import WD_TABLE_ALIGNMENT
+import datetime
+import csv
+import generator
+import webbrowser
+import subprocess
+import os
+import sys
+import shutil
+import config
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+months =["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
+
+print("Program do faktur")
+
+while True:
+    value = input("Podaj kwotę pieniedzy na jaką ma być wystawiona faktura: ")
+    value = value.replace(",",".")
+    try:
+        float(value)
+    except ValueError:
+        continue
+    break
+
+while True:
+    month = input("Podaj miesiąc w którym wykonana była usługa:")
+    month = month.lower()
+    if month in months:
+        break
+    else:
+        print("Nie ma takiego miesiąca spróbuj jeszcze raz:")
+
+########################################################################################
+
+monthnr = months.index(month) + 1
+
+today = datetime.date.today()
+year = today.strftime("%Y")
+if monthnr != 12:
+    lastday = today.replace(day=1, month=int(monthnr+1), year=int(year)) - datetime.timedelta(days=1)
+elif monthnr == 12:
+    lastday = today.replace(day=1, month=1, year=int(year)+1) - datetime.timedelta(days=1)
+lastdayofmonth = lastday.strftime("%d/%m/%Y")
+lastdayofmonth2 = lastday.strftime("%m/%Y")
+term = lastday + datetime.timedelta(days=14)
+termofpayment = term.strftime("%d/%m/%Y")
+
+name = "usługi medyczne wykonane w miesiącu - "+ month + " "+year+""
+netto = value
+
+with open('inv_nr.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    list = list(reader)
+    for i in list:
+        if i.get("month") == str(monthnr):
+            y = int(i['nr_inv'])
+            i['nr_inv'] = str(y+1)
+            data = i['nr_inv']
+
+with open('inv_nr.csv', "w", newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, ['month', 'nr_inv'])
+    writer.writeheader()
+    writer.writerows(list)
+
+word_value = str(generator.generator(value))
+#############################################################################
+
+def findandreplace(find, replace):
+    for paragraph in document.paragraphs:
+        if find in paragraph.text:
+            paragraph.add_run(replace).bold = True
+
+
+document = Document('template_inv.docx')
+print(data)
+findandreplace("Faktura nr FV", ""+data+"/" + lastdayofmonth2)
+findandreplace("Data wystawienia:", lastdayofmonth)
+findandreplace("Miejsce wystawienia", config.place)
+
+###################### TABLE1 #####################
+document.tables[0].cell(1, 0).text = config.name_1
+document.tables[0].cell(2, 0).text = config.adress_1
+document.tables[0].cell(3, 0).text = config.additional_adress_1
+document.tables[0].cell(4, 0).text = "NIP:" + config.nip_1
+document.tables[0].cell(5, 0).text = "E-mail:" + config.mail_1
+document.tables[0].cell(6, 0).text = "Tel.:" + config.tel_1
+document.tables[0].cell(1, 0).paragraphs[0].runs[0].font.bold = True
+
+document.tables[0].cell(1, 1).text = config.name_2
+document.tables[0].cell(2, 1).text = config.adress_2
+document.tables[0].cell(3, 1).text = config.additional_adress_2
+document.tables[0].cell(4, 1).text = "NIP:" + config. nip_2
+document.tables[0].cell(1, 1).paragraphs[0].runs[0].font.bold = True
+
+
+###################### TABLE2 #####################
+document.tables[1].cell(1, 1).text = name
+document.tables[1].cell(1, 5).text = netto
+document.tables[1].cell(1, 7).text = netto
+document.tables[1].cell(1, 9).text = netto
+document.tables[1].cell(1, 5).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[1].cell(1, 7).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[1].cell(1, 9).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+###################### TABLE3 #####################
+document.tables[2].cell(1, 1).text = netto
+document.tables[2].cell(1, 3).text = netto
+document.tables[2].cell(2, 1).text = netto
+document.tables[2].cell(2, 3).text = netto
+document.tables[2].cell(1, 1).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[2].cell(1, 3).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[2].cell(2, 1).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[2].cell(2, 3).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+document.tables[3].cell(0, 3).text = value+" PLN"
+document.tables[3].cell(2, 3).text = value+" PLN"
+document.tables[3].cell(3, 3).text = word_value
+document.tables[3].cell(1 ,1).text = termofpayment
+document.tables[3].cell(2 ,1).text = config.bank_name
+document.tables[3].cell(3 ,1).text = config.bank_account
+
+
+document.tables[4].cell(0 ,0).text = config.name_of_issuing
+document.tables[4].cell(0, 0).paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+document.tables[4].cell(0, 0).paragraphs[0].runs[0].font.bold = True
+
+nameoffile = "faktura rehasport nr "+data+" - "+ month +" "+ year
+document.save(nameoffile+".docx")
+output = subprocess.check_output(['libreoffice', '--convert-to', 'pdf', nameoffile+".docx"])
+webbrowser.open(nameoffile+".pdf", new=1)
+
+
+dest_fpath = config.path
+
+while True:
+    approve = input("Czy faktura jest ok? y/n: ")
+    approve = approve.lower()
+    if approve == "y":
+        os.makedirs(os.path.dirname(dest_fpath+month+"/"), exist_ok=True)
+        shutil.move(nameoffile+".docx", dest_fpath+month+"/"+nameoffile+".docx")
+        shutil.move(nameoffile+".pdf", dest_fpath+month+"/"+nameoffile+".pdf")
+        break
+    elif approve == "n":
+        while True:
+            x = input("Wybierz opcję: \ndel - usuń plik \nedit - zostaw plik .docx: ")
+            if x == "del":
+                os.remove(nameoffile+".docx")
+                os.remove(nameoffile+".pdf")
+                print("Usunięto wszystkie pliki")
+                break
+            if x == "edit":
+                os.remove(nameoffile+".pdf")
+                print("Popraw błędy w pliku .docx")
+                break
+            else:
+                continue
+        break
+    else:
+        continue
